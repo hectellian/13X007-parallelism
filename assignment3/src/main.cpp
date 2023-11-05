dc2bc84d043ca1d10310a7f2288f2a626d53d654
Anthony Christoforou <christoforouanthony@gmail.com>
2023-11-05 12:27:23 +0100

    Backup: 2023-11-05 12:27:23

--- assignment3/src/main.cpp
+++ assignment3/src/main.cpp
@@ -6,13 +6,13 @@
 #include "utils.h"
 
 // Define the domain size and simulation parameters
-int rows = 64;
-int cols = 64;
+int rows = 8;
+int cols = 8;
 const double alpha = 0.01; // Thermal diffusivity
 const double dx = 0.1;
 const double dy = 0.1;
 const double dt = 0.01;
-const int time_steps = 100000; // Number of time steps to simulate
+const int time_steps = 10e5; // Number of time steps to simulate
 
 // Function to initialize the temperature grid
 void initialize_temperature(std::vector<std::vector<double>> &grid, int rank,
