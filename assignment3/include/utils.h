2ce9a3ae6d204f67ceaa2ec3b722efe69c482048
Anthony Christoforou <christoforouanthony@gmail.com>
2023-11-07 20:19:44 +0100

    Backup: 2023-11-07 20:19:44

--- assignment3/include/utils.h
+++ assignment3/include/utils.h
@@ -6,5 +6,6 @@
 
 // Function to save the final grid to a file
 void save_grid(const std::vector<double>& grid, int rows, int cols, int rank, const std::string& grid_type = "final");
+void write_to_bmp(int N, std::vector<std::vector<double>>& data, int iter, double minval, double maxval);
 
 #endif // UTILS_H
--- assignment3/src/main.cpp
+++ assignment3/src/main.cpp
@@ -8,7 +8,7 @@
 // Define the domain size and simulation parameters
 int rows = 8;
 int cols = 8;
-const double alpha = 0.01; // Thermal diffusivity
+const double alpha = 1e-4; // Thermal diffusivity
 const double dx = 0.1;
 const double dy = 0.1;
 const double dt = 0.01;
@@ -17,19 +17,23 @@ const int time_steps = 1e5; // Number of time steps to simulate
 // Function to initialize the temperature grid
 void initialize_temperature(std::vector<std::vector<double>> &grid, int rank,
                             int rows_per_rank, int total_rows) {
+  // The 'total_rows' argument represents the total number of rows in the global grid.
   for (int i = 0; i < rows_per_rank; ++i) {
     for (int j = 0; j < cols; ++j) {
-      // Set the edges to 1
-      if (i == 0 && rank == 0) { // Top edge of the global domain
+      // Set the top edge of the first rank to 1
+      if (i == 0 && rank == 0) {
         grid[i][j] = 1.0;
-      } else if (i == rows_per_rank - 1 &&
-                 rank == total_rows / rows_per_rank -
-                             1) { // Bottom edge of the global domain
+      } 
+      // Set the bottom edge of the last rank to 1
+      else if (i == rows_per_rank - 1 && rank == (total_rows / rows_per_rank) - 1) {
         grid[i][j] = 1.0;
-      } else if (j == 0 ||
-                 j == cols - 1) { // Left or right edge of the global domain
+      }
+      // Set the left and right edges to 1 for all ranks
+      else if (j == 0 || j == cols - 1) {
         grid[i][j] = 1.0;
-      } else { // Interior points
+      } 
+      // Interior points are set to 0
+      else {
         grid[i][j] = 0.0;
       }
     }
@@ -39,8 +43,10 @@ void initialize_temperature(std::vector<std::vector<double>> &grid, int rank,
 // Function to update the temperature grid using the FTCS scheme
 void update_temperature(std::vector<std::vector<double>> &grid,
                         std::vector<double> &top_buffer,
-                        std::vector<double> &bottom_buffer, int rows_per_rank) {
+                        std::vector<double> &bottom_buffer, int rank,
+                        int rows_per_rank, int total_rows) {
   std::vector<std::vector<double>> new_grid = grid;
+  // Only iterate over the interior cells
   for (int i = 1; i < rows_per_rank - 1; ++i) {
     for (int j = 1; j < cols - 1; ++j) {
       double temp_x =
@@ -50,28 +56,32 @@ void update_temperature(std::vector<std::vector<double>> &grid,
       new_grid[i][j] = grid[i][j] + alpha * dt * (temp_x + temp_y);
     }
   }
-  // Update the top and bottom edges if they are not boundary edges
-  if (!top_buffer.empty()) {
+
+  // Update the top and bottom rows only if they are not the domain boundary
+  if (rank != 0) {
+    // Update the top interior row with the top buffer values
+    int i = 0; // top row
     for (int j = 1; j < cols - 1; ++j) {
-      double temp_x =
-          (grid[1][j] - 2.0 * grid[0][j] + top_buffer[j]) / (dx * dx);
-      double temp_y =
-          (grid[0][j + 1] - 2.0 * grid[0][j] + grid[0][j - 1]) / (dy * dy);
-      new_grid[0][j] = grid[0][j] + alpha * dt * (temp_x + temp_y);
-    }
-  }
-  if (!bottom_buffer.empty()) {
-    int i = rows_per_rank - 1;
-    for (int j = 1; j < cols - 1; ++j) {
-      double temp_x =
-          (bottom_buffer[j] - 2.0 * grid[i][j] + grid[i - 1][j]) / (dx * dx);
-      double temp_y =
-          (grid[i][j + 1] - 2.0 * grid[i][j] + grid[i][j - 1]) / (dy * dy);
+      double temp_x = (grid[i + 1][j] - 2.0 * grid[i][j] + top_buffer[j]) / (dx * dx);
+      double temp_y = (grid[i][j + 1] - 2.0 * grid[i][j] + grid[i][j - 1]) / (dy * dy);
       new_grid[i][j] = grid[i][j] + alpha * dt * (temp_x + temp_y);
     }
   }
-  grid = new_grid;
+
+  if (rank != total_rows / rows_per_rank - 1) {
+    // Update the bottom interior row with the bottom buffer values
+    int i = rows_per_rank - 1; // bottom row
+    for (int j = 1; j < cols - 1; ++j) {
+      double temp_x = (bottom_buffer[j] - 2.0 * grid[i][j] + grid[i - 1][j]) / (dx * dx);
+      double temp_y = (grid[i][j + 1] - 2.0 * grid[i][j] + grid[i][j - 1]) / (dy * dy);
+      new_grid[i][j] = grid[i][j] + alpha * dt * (temp_x + temp_y);
+    }
+  }
+
+  // Assign the updated grid back to the original grid reference
+  grid = std::move(new_grid);
 }
+
 
 // Main program
 int main(int argc, char* argv[]) {
@@ -113,48 +123,43 @@ int main(int argc, char* argv[]) {
     // Initialize the local grid with initial temperature values
     initialize_temperature(local_grid, rank, rows_per_rank, rows);
 
-    // Gather the initial temperature distribution at the root process
-    std::vector<double> gathered_grid;
-    if (rank == 0) {
-        gathered_grid.resize(rows * cols);
-    }
     // Convert local grid to a single vector for MPI communication
     std::vector<double> local_grid_linear(rows_per_rank * cols);
     for (int i = 0; i < rows_per_rank; ++i) {
         std::copy(local_grid[i].begin(), local_grid[i].end(), local_grid_linear.begin() + i * cols);
     }
-    MPI_Gather(local_grid_linear.data(), rows_per_rank * cols, MPI_DOUBLE, gathered_grid.data(), rows_per_rank * cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // Root process writes the initial grid to a file
-    if (rank == 0) {
-        save_grid(gathered_grid, rows, cols, rank, "initial");
-    }
-
+    // Create buffers for non-blocking communications
+    
     std::vector<double> top_buffer(cols, 0.0), bottom_buffer(cols, 0.0);
-    MPI_Request top_request, bottom_request;
+    MPI_Request top_send_request, top_recv_request;
+    MPI_Request bottom_send_request, bottom_recv_request;
+    
     double start_time = MPI_Wtime();
 
     // Simulation loop
     for (int step = 0; step < time_steps; ++step) {
         // Perform non-blocking sends and receives for the top and bottom rows
         if (rank > 0) {
-            MPI_Isend(local_grid[0].data(), cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &top_request);
-            MPI_Irecv(top_buffer.data(), cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &top_request);
+            MPI_Isend(local_grid[0].data(), cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &top_send_request);
+            MPI_Irecv(top_buffer.data(), cols, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &top_recv_request);
         }
         if (rank < size - 1) {
-            MPI_Isend(local_grid[rows_per_rank - 1].data(), cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &bottom_request);
-            MPI_Irecv(bottom_buffer.data(), cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &bottom_request);
+            MPI_Isend(local_grid[rows_per_rank - 1].data(), cols, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &bottom_send_request);
+            MPI_Irecv(bottom_buffer.data(), cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &bottom_recv_request);
         }
 
         // Update the local grid
-        update_temperature(local_grid, top_buffer, bottom_buffer, rows_per_rank);
+        update_temperature(local_grid, top_buffer, bottom_buffer, rank, rows_per_rank, rows);
 
         // Wait for non-blocking communications to complete
         if (rank > 0) {
-            MPI_Wait(&top_request, MPI_STATUS_IGNORE);
+            MPI_Wait(&top_send_request, MPI_STATUS_IGNORE);
+            MPI_Wait(&top_recv_request, MPI_STATUS_IGNORE);
         }
         if (rank < size - 1) {
-            MPI_Wait(&bottom_request, MPI_STATUS_IGNORE);
+            MPI_Wait(&bottom_send_request, MPI_STATUS_IGNORE);
+            MPI_Wait(&bottom_recv_request, MPI_STATUS_IGNORE);
         }
     }
 
@@ -163,16 +168,28 @@ int main(int argc, char* argv[]) {
         std::cout << "Simulation took " << (end_time - start_time) << " seconds." << std::endl;
     }
 
+    for (int i = 0; i < rows_per_rank; ++i) {
+        std::copy(local_grid[i].begin(), local_grid[i].end(), local_grid_linear.begin() + i * cols);
+    }
+
     // Gather the final temperature distribution at the root rank
     std::vector<double> final_grid;
     if (rank == 0) {
         final_grid.resize(rows * cols);
     }
-    MPI_Gather(local_grid.data()->data(), rows_per_rank * cols, MPI_DOUBLE, final_grid.data(), rows_per_rank * cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // Output the final grid to a file or the terminal
+    MPI_Gather(local_grid_linear.data(), rows_per_rank * cols, MPI_DOUBLE, final_grid.data(), rows_per_rank * cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Convert the final 1D grid to a 2D grid and save as BMP
     if (rank == 0) {
-        save_grid(final_grid, rows, cols, rank);
+        std::vector<std::vector<double>> final_2d_grid(rows, std::vector<double>(cols));
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                final_2d_grid[i][j] = final_grid[i * cols + j];
+            }
+        }
+        
+        write_to_bmp(rows, final_2d_grid, time_steps, 0, 1);
     }
 
     MPI_Finalize();
