e796047d9a28725bfb44dc52657a6ba9c06e273c
Anthony Christoforou <anthony.christoforou@etu.unige.ch>
2023-11-08 13:21:03 +0100

    backup

--- assignment3/src/main.cpp
+++ assignment3/src/main.cpp
@@ -8,11 +8,7 @@
 // Define the domain size and simulation parameters
 int rows = 8;
 int cols = 8;
-const double alpha = 1e-4; // Thermal diffusivity
-const double dx = 0.1;
-const double dy = 0.1;
-const double dt = 0.01;
-const int time_steps = 1e5; // Number of time steps to simulate
+int time_steps = 1e5; // Number of iter
 
 // Function to initialize the temperature grid
 void initialize_temperature(std::vector<std::vector<double>> &grid, int rank,
@@ -41,47 +37,52 @@ void initialize_temperature(std::vector<std::vector<double>> &grid, int rank,
 }
 
 // Function to update the temperature grid using the FTCS scheme
-void update_temperature(std::vector<std::vector<double>> &grid,
-                        std::vector<double> &top_buffer,
-                        std::vector<double> &bottom_buffer, int rank,
-                        int rows_per_rank, int total_rows) {
-  std::vector<std::vector<double>> new_grid = grid;
-  // Only iterate over the interior cells
+void update_temperature(std::vector<std::vector<double>>& grid,
+                        std::vector<double>& top_buffer,
+                        std::vector<double>& bottom_buffer,
+                        int rank, int rows_per_rank, int total_rows) {
+  // New parameters as provided
+  double hx = 1.0 / total_rows; 
+  double hy = 1.0 / total_rows; 
+  double C = 1.0; 
+  double dt = 0.25 * hx * hx / C; 
+
+  double diagx = -2.0 + hx * hx / (2 * C * dt);
+  double diagy = -2.0 + hy * hy / (2 * C * dt);
+  double weightx = C * dt / (hx * hx);
+  double weighty = C * dt / (hy * hy);
+
+  std::vector<std::vector<double>> new_grid = grid; // This will store the new values
+
+  // Update interior cells
   for (int i = 1; i < rows_per_rank - 1; ++i) {
     for (int j = 1; j < cols - 1; ++j) {
-      double temp_x =
-          (grid[i + 1][j] - 2.0 * grid[i][j] + grid[i - 1][j]) / (dx * dx);
-      double temp_y =
-          (grid[i][j + 1] - 2.0 * grid[i][j] + grid[i][j - 1]) / (dy * dy);
-      new_grid[i][j] = grid[i][j] + alpha * dt * (temp_x + temp_y);
+      new_grid[i][j] = weightx * (grid[i - 1][j] + grid[i + 1][j] + grid[i][j] * diagx) +
+                       weighty * (grid[i][j - 1] + grid[i][j + 1] + grid[i][j] * diagy);
     }
   }
 
-  // Update the top and bottom rows only if they are not the domain boundary
+  // Update top interior row with the top buffer if not at domain boundary
   if (rank != 0) {
-    // Update the top interior row with the top buffer values
-    int i = 0; // top row
+    int i = 0; // Top row
     for (int j = 1; j < cols - 1; ++j) {
-      double temp_x = (grid[i + 1][j] - 2.0 * grid[i][j] + top_buffer[j]) / (dx * dx);
-      double temp_y = (grid[i][j + 1] - 2.0 * grid[i][j] + grid[i][j - 1]) / (dy * dy);
-      new_grid[i][j] = grid[i][j] + alpha * dt * (temp_x + temp_y);
+      new_grid[i][j] = weightx * (top_buffer[j] + grid[i + 1][j] + grid[i][j] * diagx) +
+                       weighty * (grid[i][j - 1] + grid[i][j + 1] + grid[i][j] * diagy);
     }
   }
 
-  if (rank != total_rows / rows_per_rank - 1) {
-    // Update the bottom interior row with the bottom buffer values
-    int i = rows_per_rank - 1; // bottom row
+  // Update bottom interior row with the bottom buffer if not at domain boundary
+  if (rank != (total_rows / rows_per_rank) - 1) {
+    int i = rows_per_rank - 1; // Bottom row
     for (int j = 1; j < cols - 1; ++j) {
-      double temp_x = (bottom_buffer[j] - 2.0 * grid[i][j] + grid[i - 1][j]) / (dx * dx);
-      double temp_y = (grid[i][j + 1] - 2.0 * grid[i][j] + grid[i][j - 1]) / (dy * dy);
-      new_grid[i][j] = grid[i][j] + alpha * dt * (temp_x + temp_y);
+      new_grid[i][j] = weightx * (grid[i - 1][j] + bottom_buffer[j] + grid[i][j] * diagx) +
+                       weighty * (grid[i][j - 1] + grid[i][j + 1] + grid[i][j] * diagy);
     }
   }
 
-  // Assign the updated grid back to the original grid reference
-  grid = std::move(new_grid);
+  // Swap the new grid with the old grid to prepare for the next iteration
+  grid.swap(new_grid);
 }
-
 
 // Main program
 int main(int argc, char* argv[]) {
@@ -91,18 +92,20 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     // Check command-line arguments
-    if (argc == 2) {
+    if (argc == 3) {
         rows = cols = std::atoi(argv[1]);
-        if (rows <= 0) {
+        time_steps = std::atoi(argv[2]);
+        
+        if (rows <= 0 || time_steps <= 0) {
             if (rank == 0) {
-                std::cerr << "Invalid grid size. Please provide a positive integer." << std::endl;
+                std::cerr << "Invalid grid size or time steps. Please provide positive integers." << std::endl;
             }
             MPI_Finalize();
             return 1;
         }
-    } else if (argc > 2) {
+    } else {
         if (rank == 0) {
-            std::cerr << "Usage: " << argv[0] << " <grid_size>" << std::endl;
+            std::cerr << "Usage: " << argv[0] << " <grid_size> <time_steps>" << std::endl;
         }
         MPI_Finalize();
         return 1;
@@ -123,17 +126,9 @@ int main(int argc, char* argv[]) {
     // Initialize the local grid with initial temperature values
     initialize_temperature(local_grid, rank, rows_per_rank, rows);
 
-    // Convert local grid to a single vector for MPI communication
-    std::vector<double> local_grid_linear(rows_per_rank * cols);
-    for (int i = 0; i < rows_per_rank; ++i) {
-        std::copy(local_grid[i].begin(), local_grid[i].end(), local_grid_linear.begin() + i * cols);
-    }
-
     // Create buffers for non-blocking communications
-    
     std::vector<double> top_buffer(cols, 0.0), bottom_buffer(cols, 0.0);
-    MPI_Request top_send_request, top_recv_request;
-    MPI_Request bottom_send_request, bottom_recv_request;
+    MPI_Request top_request, bottom_request;
     
     double start_time = MPI_Wtime();
 
@@ -141,12 +136,12 @@ int main(int argc, char* argv[]) {
     for (int step = 0; step < time_steps; ++step) {
         // Perform non-blocking sends and receives for the top and bottom rows
         if (rank > 0) {
-            MPI_Isend(local_grid[0].data(), cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &top_send_request);
-            MPI_Irecv(top_buffer.data(), cols, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &top_recv_request);
+            MPI_Isend(local_grid[0].data(), cols, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &top_request);
+            MPI_Irecv(top_buffer.data(), cols, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &top_request);
         }
         if (rank < size - 1) {
-            MPI_Isend(local_grid[rows_per_rank - 1].data(), cols, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &bottom_send_request);
-            MPI_Irecv(bottom_buffer.data(), cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &bottom_recv_request);
+            MPI_Isend(local_grid[rows_per_rank - 1].data(), cols, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &bottom_request);
+            MPI_Irecv(bottom_buffer.data(), cols, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &bottom_request);
         }
 
         // Update the local grid
@@ -154,12 +149,10 @@ int main(int argc, char* argv[]) {
 
         // Wait for non-blocking communications to complete
         if (rank > 0) {
-            MPI_Wait(&top_send_request, MPI_STATUS_IGNORE);
-            MPI_Wait(&top_recv_request, MPI_STATUS_IGNORE);
+            MPI_Wait(&top_request, MPI_STATUS_IGNORE);
         }
         if (rank < size - 1) {
-            MPI_Wait(&bottom_send_request, MPI_STATUS_IGNORE);
-            MPI_Wait(&bottom_recv_request, MPI_STATUS_IGNORE);
+            MPI_Wait(&bottom_request, MPI_STATUS_IGNORE);
         }
     }
 
@@ -168,6 +161,7 @@ int main(int argc, char* argv[]) {
         std::cout << "Simulation took " << (end_time - start_time) << " seconds." << std::endl;
     }
 
+    std::vector<double> local_grid_linear(rows_per_rank * cols);
     for (int i = 0; i < rows_per_rank; ++i) {
         std::copy(local_grid[i].begin(), local_grid[i].end(), local_grid_linear.begin() + i * cols);
     }
